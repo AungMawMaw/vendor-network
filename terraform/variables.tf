ee7acb7f1c940f5d7375aeadd8fb065b77561a10
AungMawMaw <aungmaw.bc@gmail.com>
2024-03-22 02:39:48 +0630

    adding terraform main.tf and variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,18 @@
 variable "aws_region" {}
 
 variable "app_name" {
-  description: "name of app",
-  default: "vendor-network"
+  description: "name of app"
+  default= "vendor-network"
 }
 
 variable "public_subnets" {
-  default: ["10.10.100.0/24", "10.10.101.0/24"]
+  default= ["10.10.100.0/24", "10.10.101.0/24"]
 }
 
 variable "private_subnets" {
-  default: ["10.10.0.0/24", "10.10.1.0/24"]
+  default= ["10.10.0.0/24", "10.10.1.0/24"]
 }
 
 variable "availability_zones" {
-  default: ["ap-southeast-1a", "ap-southeast-1b"]
+  default= ["ap-southeast-1a", "ap-southeast-1b"]
 }
